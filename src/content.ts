ab97e3b8a3b8e2ff480579403786eb2f3f60f101
TaikiTkayanagi <ikiat4@gmail.com>
2023-07-11 08:35:32 +0900

    ちょこっと修正

--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,7 @@
-const subTitlesDiv = document.getElementsByClassName("captions-display--captions-cue-text--ECkJu");
-
-console.log(subTitlesDiv[0].textContent);
+console.log("開始")
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("開始2")
+    const subTitlesDiv = document.getElementsByClassName("captions-display--captions-cue-text--ECkJu");
+    console.log(subTitlesDiv[0].textContent);
+})
+console.log("終了")
